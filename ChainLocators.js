c65659e3d3dfa0702764b5a33e30847d3fd6e761
noerm <noerm177@gmail.com>
2018-07-10 01:22:29 -0500

    nth child

--- ChainLocators.js
+++ ChainLocators.js
@@ -4,11 +4,11 @@ describe('Chain locators demo', () => {
         
         browser.get('https://juliemr.github.io/protractor-demo/');
         //repeater, chain and css
-        element(by.model('first').sendKeys("3"));
-        element(by.model('second').sendKeys("5"));
+        element(by.model('first')).sendKeys("3");
+        element(by.model('second')).sendKeys("5");
 
     element(by.id('gobutton')).click();
-    element(by.repeater('result in memory')).element(by.class('td:nth-child(2)')).getText().then(function(text)
+    element(by.repeater('result in memory')).element(by.css('td:nth-child(3)')).getText().then(function(text)
         {
             console.log(text);
         })
