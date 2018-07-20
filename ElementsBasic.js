243b016072a82ecd9f55fc779c0749c26c6a51d3
Noerm <noerm177@gmail.com>
2018-07-20 15:31:43 -0500

    Add element basic

---
+++ ElementsBasic.js
@@ -0,0 +1,18 @@
+describe('Protractor Element Demo', () => {
+    
+    it('Locators', () => {
+        
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+            element(by.model('first')).sendKeys('3');
+            element(by.model('second')).sendKeys("5");
+            element(by.id('gobutton')).click();
+
+        expect(element(by.css("h2[class= 'ng-binding']")).getText()).toBe("8");
+        
+            element(by.css("h2[class= 'ng-binding']")).getText().then(function(text){
+                console.log(text);
+        })
+
+    })
+});
